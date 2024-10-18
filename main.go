e1b6556aa04871940d4756ef9076f04f4d5c2134
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2024-10-18 13:57:50 +0700

    feat: echo bot

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 package main
 
 import (
-	"fmt"
-
-	"example.com/yahfaz/utils"
+	"example.com/yahfaz/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -29,11 +14,11 @@ func main() {
 		panic(err.Error())
 	}
 
-	token, err := utils.GenerateJWTToken()
+	r := gin.Default()
 
-	if err != nil {
-		panic(err.Error())
-	}
+	routes.RegisterRoutes(r)
 
-	fmt.Println(token)
+	port := ":5050"
+
+	r.Run(port)
 }
